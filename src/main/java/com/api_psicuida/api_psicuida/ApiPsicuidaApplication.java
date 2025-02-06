2dd672c362885c646467429ac2d1164e4231d34e
andreza.freitas <andreza1freitas@gmail.com>
2025-02-06 20:50:28 -0300

    Initial commit

---
+++ src/main/java/com/api_psicuida/api_psicuida/ApiPsicuidaApplication.java
@@ -0,0 +1,13 @@
+package com.api_psicuida.api_psicuida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPsicuidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPsicuidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_psicuida/api_psicuida/ApiPsicuidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_psicuida.api_psicuida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPsicuidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
